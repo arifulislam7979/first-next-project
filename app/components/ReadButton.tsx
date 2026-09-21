07a3971d828c88956e97c34abd5d6a92ef8a1ed9
ariful797988 <arifislam01760083176@gmail.com>
2026-09-21 21:21:09 +0600

    new commit

--- app/components/ReadButton.tsx
+++ app/components/ReadButton.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useContext } from "react";
 import { IBook } from "../type";
@@ -6,21 +6,25 @@ import { bookContext } from "../context/BooksContext";
 import { toast } from "react-toastify";
 
 interface ReadButtonProps {
-    book: IBook
+  book: IBook;
 }
-const ReadButton = ({book}:ReadButtonProps) => {
-    const {readBooks, setReadBooks} = useContext(bookContext)
-    
-    const handleReadBook = () => {
-        console.log('read' , book);
-        setReadBooks([...readBooks, book])
-        toast.success(`You have read ${book.bookName}`)
-    }
-    return (
-        <div>
-            <button className="btn btn-primary" onClick={()=> handleReadBook()}> 📖 Read Book </button>
-        </div>
-    );
+const ReadButton = ({ book }: ReadButtonProps) => {
+  const { readBooks, setReadBooks } = useContext(bookContext);
+
+  const handleReadBook = () => {
+    console.log("read", book);
+    setReadBooks([...readBooks, book]);
+    toast.success(`You have read ${book.bookName}`);
+  };
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => handleReadBook()}>
+        {" "}
+        📖 Read Book
+      </button>
+      {/* testing */}
+    </div>
+  );
 };
 
 export default ReadButton;
